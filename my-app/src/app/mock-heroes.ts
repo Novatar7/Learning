1e18c3ccffc6d4c6dd00492f04ff4f041bbf772e
Novatar7 <np03s180025@heraldcollege.edu.np>
2024-05-22 10:37:17 +0545

    Done with tour of heroes

--- my-app/src/app/mock-heroes.ts
+++ my-app/src/app/mock-heroes.ts
@@ -1,13 +1,24 @@
-import { Hero } from "./hero";
+import {Hero} from "./hero";
 
-export const  HEROES: Hero[]=[
-    { id: 12, name: 'Dr. Nice' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr. IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Alucard'},
+  {id: 2, name: 'Argus'},
+  {id: 3, name: 'Aron'},
+  {id: 4, name: 'Boras'},
+  {id: 5, name: 'Bora'},
+  {id: 6, name: 'Cecelian'},
+  {id: 7, name: 'Fanny'},
+  {id: 8, name: 'Karrie'},
+  {id: 9, name: 'Granger'},
+  {id: 10, name: 'Kimmy'},
+  {id: 11, name: 'Mary'},
+  {id: 12, name: 'Dr. Nice'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr. IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
